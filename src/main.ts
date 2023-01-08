e568738b24ef06169f3fa42ea75df895f60b0a1d
Wojciech Iskierka <ext-wojciech.iskierka@terveystalo.com>
2023-01-08 20:40:27 +0100

    added logger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger('bootstrap');
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
+  logger.log(
+    `Application listening on the port ${
+      !process.env.PORT ? '3000' : process.env.PORT
+    }`,
+  );
 }
 bootstrap();
